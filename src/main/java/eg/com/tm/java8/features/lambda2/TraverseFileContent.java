aa9f8d8ef6b7f6019a1d0d5361d47e746cad8adb
Mohamed Taman <mohamed.taman@gmail.com>
2014-08-24 01:28:38 +0300

    Traversing, and filtering collections with Lambda

---
+++ src/main/java/eg/com/tm/java8/features/lambda2/TraverseFileContent.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2014 mohamed_taman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eg.com.tm.java8.features.lambda2;
+
+import java.io.IOException;
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import java.nio.file.Path;
+import static java.nio.file.Paths.get;
+import java.util.Iterator;
+import java.util.List;
+import static java.util.logging.Level.SEVERE;
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+
+/**
+ *
+ * @author mohamed_taman
+ */
+public class TraverseFileContent {
+
+    private static final Logger logger = getLogger(TraverseFileContent.class.getName());
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+
+            Path path = get("src/main", "resources", "data.txt");
+
+            if (exists(path, NOFOLLOW_LINKS)) {
+
+                logger.config(path.toAbsolutePath().toString());
+
+                List<String> lines = readAllLines(path);
+
+                // Traversing with for each
+                for (String line : lines) {
+                    logger.info(line);
+                }
+
+                // Traversing with iterator
+                Iterator<String> itr = lines.iterator();
+
+                while (itr.hasNext()) {
+                    logger.info(itr.next());
+                }
+
+                // Traversing with lambda new forEach
+                lines.forEach(line -> logger.info(line));
+
+                // or with streams
+                lines.stream().forEach(line -> logger.info(line));
+                
+            } else {
+                
+                logger.log(SEVERE, "{0} , Doesn't exists", path.toAbsolutePath());
+            }
+        }
+        catch (IOException ex) {
+            logger.severe(ex.getMessage());
+        }
+
+    }
+
+}
---
+++ src/main/java/eg/com/tm/java8/features/lambda2/predicate/FileDirFilter.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2014 mohamed_taman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eg.com.tm.java8.features.lambda2.predicate;
+
+import java.io.IOException;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.out;
+import java.nio.file.DirectoryStream;
+import static java.nio.file.Files.getAttribute;
+import static java.nio.file.Files.isDirectory;
+import static java.nio.file.Files.isHidden;
+import static java.nio.file.Files.newDirectoryStream;
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import java.nio.file.Path;
+import static java.nio.file.Paths.get;
+import java.nio.file.attribute.FileTime;
+import static java.nio.file.attribute.FileTime.fromMillis;
+import static java.util.concurrent.TimeUnit.DAYS;
+import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+
+/**
+ *
+ * @author mohamed_taman
+ */
+public class FileDirFilter {
+
+    private static final Path path = get("src/main", "resources", "eg/com/tm/java8/features/lambda2");
+    
+    private static final Logger logger = getLogger(FileDirFilter.class.getName());
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        predicateInInnerClass();
+        predicateWithLambda();
+
+    }
+
+    private static void predicateInInnerClass() {
+
+        Predicate<Path> dirsFilter = new Predicate<Path>() {
+
+            @Override
+            public boolean test(Path t) {
+
+                return isDirectory(t, NOFOLLOW_LINKS);
+
+            }
+        };
+
+        try (DirectoryStream<Path> dirStream = newDirectoryStream(path)) {
+            for (Path file : dirStream) {
+                if (dirsFilter.test(file)) {
+                    out.println(file.getFileName());
+                }
+            }
+        }
+        catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private static void predicateWithLambda() {
+
+        Predicate<Path> noFilter = (p) -> true;
+
+        logger.info("All Path contents");
+        filterPath(noFilter);
+        out.println("--------------------------------------------");
+
+        Predicate<Path> dirFilter = (p) -> isDirectory(p, NOFOLLOW_LINKS);
+
+        logger.info("Print dirs only");
+        filterPath(dirFilter);
+        out.println("--------------------------------------------");
+
+        Predicate<Path> hiddenFilter = (p) -> {
+            boolean hidden = false;
+            try {
+                hidden = isHidden(p);
+            }
+            catch (IOException e) {
+            }
+            return hidden;
+        };
+
+        logger.info("Print hidden files/dirs only");
+        filterPath(hiddenFilter);
+        out.println("--------------------------------------------");
+
+        Predicate<Path> timeFilter = (p) -> {
+            long currentTime = 0;
+            long modifiedTime = 0;
+            try {
+                currentTime = fromMillis(currentTimeMillis()).to(DAYS);
+                modifiedTime = ((FileTime) getAttribute(p, "basic:lastModifiedTime",
+                        NOFOLLOW_LINKS)).to(DAYS);
+
+            }
+            catch (IOException e) {
+            }
+            System.out.println(currentTime);
+             System.out.println(modifiedTime);
+            return currentTime == modifiedTime;
+        };
+
+        logger.info("Print today modified files/dirs only");
+        filterPath(timeFilter);
+        logger.info("--------------------------------------------");
+
+    }
+
+    private static void filterPath(Predicate<Path> pred) {
+        try {
+            newDirectoryStream(path).forEach(p -> {
+                if (pred.test(p)) {
+                    out.println(p.getFileName());
+                }
+            });
+        }
+        catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
